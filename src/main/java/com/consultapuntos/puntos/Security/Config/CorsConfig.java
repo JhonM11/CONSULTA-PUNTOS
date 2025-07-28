51a7b01de40172b3f16001f355244001043659b6
John Morales <jhonfremorales500@gmail.com>
2025-07-28 15:22:49 -0500

    feat: ajuste congif CORS

--- src/main/java/com/consultapuntos/puntos/Security/Config/CorsConfig.java
+++ src/main/java/com/consultapuntos/puntos/Security/Config/CorsConfig.java
@@ -16,11 +16,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("http://localhost:5173") // Permite solo el origen de tu frontend
+                        .allowedOrigins("*") // Permite solo el origen de tu frontend
+                        .allowedHeaders("Content-Type", "Authorization")// Encabezados específicos
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                        .allowedHeaders("Content-Type", "Authorization", "api-key")
                         .exposedHeaders("Content-Disposition")
-                        .allowCredentials(true)  // Permite el uso de cookies o Authorization
                         .maxAge(3600); // Tiempo de cache del preflight en segundos
             }
         };
